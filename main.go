96000111aad887215667cc8eb6aca142ee8f6db0
Menghan Li <menghanl@google.com>
2018-05-14 16:40:59 -0700

    parse flags and do logs

--- main.go
+++ main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
+
+	log "github.com/sirupsen/logrus"
+)
+
+var (
+	token   = flag.String("token", "", "github token")
+	release = flag.String("release", "", "major release number, for example 1.12")
+	owner   = flag.String("owner", "grpc", "github repo owner")
+	repo    = flag.String("repo", "grpc-go", "github repo")
+)
+
+type client struct {
+	c *github.Client
+}
+
+func (c *client) newBranch(ctx context.Context, branchName string) error {
+	log.Infof("creating branch: %v", branchName)
+	return fmt.Errorf("unimplemented")
+}
 
 func main() {
-	fmt.Println("git bot")
+	flag.Parse()
+
+	if *release == "" {
+		fmt.Println("invalid release number, usage:")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	var tc *http.Client
+	if *token != "" {
+		ctx := context.Background()
+		ts := oauth2.StaticTokenSource(
+			&oauth2.Token{AccessToken: *token},
+		)
+		tc = oauth2.NewClient(ctx, ts)
+	}
+	c := &client{c: github.NewClient(tc)}
+	if err := c.newBranch(context.Background(), "v"+*release+".x"); err != nil {
+		log.Fatal(err)
+	}
 }
