96f1ea4b3202d728fbd0b1e594a7427ae095f067
Menghan Li <menghanl@google.com>
2018-05-16 18:01:18 -0700

    use billy/memfs

--- gitwrapper/repo.go
+++ gitwrapper/repo.go
@@ -5,20 +5,24 @@ import (
 	"fmt"
 
 	log "github.com/sirupsen/logrus"
-	"gopkg.in/src-d/go-git.v4"
+	billy "gopkg.in/src-d/go-billy.v4"
+	"gopkg.in/src-d/go-billy.v4/memfs"
+	git "gopkg.in/src-d/go-git.v4"
 	"gopkg.in/src-d/go-git.v4/storage/memory"
 )
 
 // Repo represends a git repo.
 type Repo struct {
-	r *git.Repository
+	r  *git.Repository
+	fs billy.Filesystem
 }
 
 // GithubClone creates a new Repo by cloning from github.
 func GithubClone(owner, repo string) (*Repo, error) {
 	url := fmt.Sprintf("https://github.com/%v/%v", owner, repo)
 
-	r, err := git.Clone(memory.NewStorage(), nil, &git.CloneOptions{
+	fs := memfs.New()
+	r, err := git.Clone(memory.NewStorage(), fs, &git.CloneOptions{
 		URL: url,
 	})
 
@@ -27,7 +31,8 @@ func GithubClone(owner, repo string) (*Repo, error) {
 	}
 
 	return &Repo{
-		r: r,
+		r:  r,
+		fs: fs,
 	}, nil
 }
 
