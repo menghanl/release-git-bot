0556526d9237b1c24284423022984e22e43214d4
Menghan Li <menghanl@google.com>
2018-05-18 15:27:39 -0700

    structure and comments

--- gitwrapper/repo.go
+++ gitwrapper/repo.go
@@ -7,13 +7,13 @@ import (
 	"time"
 
 	"gopkg.in/src-d/go-git.v4/plumbing"
+	"gopkg.in/src-d/go-git.v4/plumbing/object"
 
 	"github.com/alecthomas/template"
 	log "github.com/sirupsen/logrus"
 	billy "gopkg.in/src-d/go-billy.v4"
 	"gopkg.in/src-d/go-billy.v4/memfs"
 	git "gopkg.in/src-d/go-git.v4"
-	"gopkg.in/src-d/go-git.v4/plumbing/object"
 	"gopkg.in/src-d/go-git.v4/storage/memory"
 )
 
@@ -28,6 +28,7 @@ type Repo struct {
 // GithubClone creates a new Repo by cloning from github.
 func GithubClone(owner, repo string) (*Repo, error) {
 	url := fmt.Sprintf("https://github.com/%v/%v", owner, repo)
+	log.Infof("executing %q", "git clone "+url)
 
 	fs := memfs.New()
 	r, err := git.Clone(memory.NewStorage(), fs, &git.CloneOptions{
@@ -49,46 +50,75 @@ func GithubClone(owner, repo string) (*Repo, error) {
 	}, nil
 }
 
-func (r *Repo) createAndCheckoutBranch(name string) (*object.Commit, error) {
+func (r *Repo) createAndCheckoutBranch(name string) error {
 	ref, err := r.r.Head()
 	if err != nil {
-		return nil, fmt.Errorf("failed to call Head(): %v", err)
+		return fmt.Errorf("failed to call Head(): %v", err)
 	}
+	log.Infof("HEAD at: %v", ref)
 
-	err = r.worktree.Checkout(&git.CheckoutOptions{
+	log.Infof("executing %q", "checkout -b"+name)
+	if err := r.worktree.Checkout(&git.CheckoutOptions{
 		Hash:   ref.Hash(),
 		Branch: plumbing.ReferenceName("refs/heads/" + name),
 		Create: true,
-	})
-	if err != nil {
-		return nil, fmt.Errorf("failed to checkout to new branch: %v", err)
+	}); err != nil {
+		return fmt.Errorf("failed to checkout to new branch: %v", err)
 	}
 
-	// newRef := plumbing.NewHashReference("refs/heads/my-branch", headRef.Hash())
-
-	headCommit, err := r.r.CommitObject(ref.Hash())
+	ref, err = r.r.Head()
 	if err != nil {
-		return nil, fmt.Errorf("failed to find commit for head: %v", err)
+		return fmt.Errorf("failed to call Head(): %v", err)
 	}
-	return headCommit, nil
+	log.Infof("HEAD at: %v", ref)
+
+	commit, err := r.r.CommitObject(ref.Hash())
+	if err != nil {
+		return fmt.Errorf("failed to find commit for head: %v", err)
+	}
+	log.Infof("Commit at HEAD:\n%v", commit)
+
+	return nil
 }
 
 func (r *Repo) updateVersionFile(newVersion string) error {
+	log.Infof("executing %q", "edit version.go")
 	versionFile, err := r.fs.OpenFile("version.go", os.O_WRONLY|os.O_TRUNC, 0644)
 	if err != nil {
 		return fmt.Errorf("failed to open file version.go: %v", err)
 	}
-	defer versionFile.Close()
-
 	t := template.Must(template.New("version").Parse(versionTemplate))
 	err = t.Execute(versionFile, map[string]string{"version": newVersion})
 	if err != nil {
+		versionFile.Close()
 		return fmt.Errorf("failed to execute template to file: %v", err)
 	}
+	versionFile.Close()
+
+	status, err := r.worktree.Status()
+	if err != nil {
+		return fmt.Errorf("failed to get status from worktree: %v", err)
+	}
+	log.Infof("current worktree status (git status):\n%v", status)
+
+	log.Infof("executing %q", "git commit -m 'Change version to %v'")
+	commitMsg := fmt.Sprintf("Change version to %v", newVersion)
+	if _, err := r.worktree.Commit(commitMsg, &git.CommitOptions{
+		All: true,
+		Author: &object.Signature{
+			Name:  "release bot",
+			Email: "releasebot@grpc.io",
+			When:  time.Now(),
+		},
+	}); err != nil {
+		return fmt.Errorf("failed to commit: %v", err)
+	}
+
 	return nil
 }
 
 func (r *Repo) printDiffInHeadCommit() error {
+	log.Infof("executing %q", "git diff HEAD~")
 	headRef, err := r.r.Head()
 	if err != nil {
 		return fmt.Errorf("failed to call Head(): %v", err)
@@ -108,44 +138,25 @@ func (r *Repo) printDiffInHeadCommit() error {
 		return fmt.Errorf("failed to get patch: %v", err)
 	}
 	log.Info(patch)
+
 	return nil
 }
 
-// Try prints head.
+// Try does the work.
 func (r *Repo) Try() {
 	// git checkout -b release_version
-	headCommit, err := r.createAndCheckoutBranch("release_version")
-	if err != nil {
+	if err := r.createAndCheckoutBranch("release_version"); err != nil {
 		log.Fatal(err)
 	}
-	log.Info(headCommit.String())
 
+	// edit file
+	// git commit -m 'Change version to %v'
 	const newVersion = "1.new.0"
-
-	// make change to file
 	if err := r.updateVersionFile(newVersion); err != nil {
 		log.Fatal(err)
 	}
-	status, err := r.worktree.Status()
-	if err != nil {
-		log.Fatalf("failed to get status from worktree: %v", err)
-	}
-	log.Infof("current worktree status: \n%v", status)
 
-	// git commit -m 'Change version to %v'
-	commitMsg := fmt.Sprintf("Change version to %v", newVersion)
-	_, err = r.worktree.Commit(commitMsg, &git.CommitOptions{
-		All: true,
-		Author: &object.Signature{
-			Name:  "release bot",
-			Email: "releasebot@grpc.io",
-			When:  time.Now(),
-		},
-	})
-	if err != nil {
-		log.Fatalf("failed to commit: %v", err)
-	}
-
+	// git diff HEAD~
 	if err := r.printDiffInHeadCommit(); err != nil {
 		log.Fatal(err)
 	}
