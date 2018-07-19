6b891af17d1573e4e690eb9273547b44386b1030
Menghan Li <menghanl@google.com>
2018-07-18 17:29:54 -0700

    add several new lines

--- main.go
+++ main.go
@@ -64,7 +64,7 @@ func main() {
 	}
 	upstreamGithub := ghclient.New(transportClient, upstreamUser, *repo)
 
-	fmt.Printf("Cloning %v/%v into memory\n", *user, *repo)
+	fmt.Printf(" - Cloning %v/%v into memory\n\n", *user, *repo)
 	forkLocalGit, err := gitwrapper.GithubClone(&gitwrapper.GithubCloneConfig{
 		Owner: *user,
 		Repo:  *repo,
@@ -73,14 +73,17 @@ func main() {
 		log.Fatalf("failed to github clone: %v", err)
 	}
 
+	fmt.Println()
 	/* Step 1: create an upstream release branch if it doesn't exist */
 	upstreamReleaseBranchName := fmt.Sprintf("v%v.%v.x", ver.Major, ver.Minor)
-	fmt.Printf(" - Step 1: create an upstream release branch %v/%v/%v\n", upstreamUser, *repo, upstreamReleaseBranchName)
+	fmt.Printf(" - Step 1: create an upstream release branch %v/%v/%v\n\n", upstreamUser, *repo, upstreamReleaseBranchName)
 	upstreamGithub.NewBranchFromHead(upstreamReleaseBranchName)
 
+	fmt.Println()
 	/* Step 2: on release branch, change version file to 1.release.0 */
-	fmt.Printf(" - Step 2: on release branch, change version to %v\n", *newVersion)
+	fmt.Printf(" - Step 2: on release branch, change version to %v\n\n", *newVersion)
 	prURL1 := makePR(upstreamGithub, forkLocalGit, *newVersion, upstreamReleaseBranchName)
+	// prURL1 := "https://github.com/menghanl/grpc-go/pull/17"
 	fmt.Printf("PR %v created, merge before continuing...\n", prURL1)
 
 	/* Wait for the PR to be merged */
@@ -92,8 +95,9 @@ func main() {
 		survey.AskOne(prompt, &prMergeConfirmed, nil)
 	}
 
+	fmt.Println()
 	/* Step 3: generate release note and create draft release */
-	fmt.Printf(" - Step 3: generate release note and creating draft release\n")
+	fmt.Printf(" - Step 3: generate release note and create draft release\n\n")
 	// Get and print the markdown release notes.
 	markdownNote := releaseNote(upstreamGithub, ver)
 	// fmt.Println(markdownNote)
@@ -103,6 +107,7 @@ func main() {
 	if err != nil {
 		log.Fatal("failed to create release: ", err)
 	}
+	// releaseURL := "https://github.com/menghanl/grpc-go/release/untaged-blahblahblah"
 	fmt.Printf("Draft release %v created, publish before continuing\n", releaseURL)
 
 	/* Wait for the release to be published */
@@ -114,19 +119,23 @@ func main() {
 		survey.AskOne(prompt, &releasePublishConfirmed, nil)
 	}
 
+	fmt.Println()
 	/* Step 4: on release branch, change version file to 1.release.1-dev */
 	nextMinorRelease := ver
 	nextMinorRelease.Patch++ // Increment the pateh version, not the minor version.
 	nextMinorReleaseStr := fmt.Sprintf("%v-dev", nextMinorRelease.String())
-	fmt.Printf(" - Step 4: on release branch, change version to %v\n", nextMinorReleaseStr)
+	fmt.Printf(" - Step 4: on release branch, change version to %v\n\n", nextMinorReleaseStr)
+	// prURL2 := "https://github.com/menghanl/grpc-go/pull/18"
 	prURL2 := makePR(upstreamGithub, forkLocalGit, nextMinorReleaseStr, upstreamReleaseBranchName)
 	fmt.Println("PR to merge: ", prURL2)
 
+	fmt.Println()
 	/* Step 5: on master branch, change version file to 1.release+1.0-dev */
 	nextMajorRelease := ver
 	nextMajorRelease.Minor++ // Increment the minor version, not the major version.
 	nextMajorReleaseStr := fmt.Sprintf("%v-dev", nextMajorRelease.String())
-	fmt.Printf(" - Step 5: on master branch, change version to %v\n", nextMajorReleaseStr)
+	fmt.Printf(" - Step 5: on master branch, change version to %v\n\n", nextMajorReleaseStr)
+	// prURL3 := "https://github.com/menghanl/grpc-go/pull/19"
 	prURL3 := makePR(upstreamGithub, forkLocalGit, nextMajorReleaseStr, "master")
 	fmt.Println("PR to merge: ", prURL3)
 
